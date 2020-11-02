342bdd8ec17eeec1cd54938447100b903c4199db
ghandeland <gabriel.handeland@gmail.com>
2020-11-02 13:25:23 +0100

    Initializing VS

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kristiania.PG3302_1.CustomCardGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
