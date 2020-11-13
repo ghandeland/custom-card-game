4c1db5e0769065e28b588eef00d8768c9c4ee76b
abhil <abhilash.kamboj@gmail.com>
2020-11-13 18:45:06 +0100

    Created strategy classes for special cards resolves #7

---
+++ specialCardStrategy/VultureStrategy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kristiania.PG3302_1.CustomCardGame.specialCardStrategy
+{
+    class VultureStrategy
+    {
+    }
+}
