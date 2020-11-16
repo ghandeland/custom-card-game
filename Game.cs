0a6cbb753c19f391ce26bb331471c00d099c8b3b
Skarven1 <adrian.hansen@gmail.com>
2020-11-16 01:14:00 +0100

    added game initializing

--- Game.cs
+++ Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Diagnostics.Tracing;
 using System.Text;
 using System.Threading;
@@ -10,9 +11,32 @@ namespace Kristiania.PG3302_1.CustomCardGame
     {
         private bool GameIsWon { get; set; }
         private Dealer _dealer;
+        private List<Player> _players;
+        private Deck _deck;
 
-        public void startGame();
+        public Game(int playerAmount)
+        {
+            _deck = new Deck();
+            _dealer = new Dealer(_deck);
+            _players = new List<Player>();
+            if (playerAmount > 1 && playerAmount < 5)
+            {
+                for (int i = 0; i < playerAmount; i++)
+                {
+                    _players.Add(new Player(i+1, _dealer));
+                }
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
 
 
+
+        public void StartGame()
+        {
+            
+        }
     }
 }
